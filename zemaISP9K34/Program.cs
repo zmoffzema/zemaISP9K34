f05ed8e3c051b42f86162a17d165bc4738a2f8f4
z_moff <zmoffzema@gmail.com>
2024-06-19 23:36:15 +0300

    Добавьте файлы проекта.

---
+++ zemaISP9K34/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        int number1 = 5;
+        int number2 = 10;
+
+        int result = Sum(number1, number2);
+        Console.WriteLine("The sum is: " + result);
+    }
+
+    public static int Sum(int a, int b)
+    {
+        return a + b;
+    }
+}
